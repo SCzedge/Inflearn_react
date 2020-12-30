2668a4f7d182855a56331bd4834e760b16c91f77
sczedge <sczedge@gmail.com>
2020-12-30 22:07:12 +0900

    11_component_done

--- 11_component/src/App.js
+++ 11_component/src/App.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import Counter from "./Counter";
-export default function App() {
-  const [color, setColor] = useState("red");
 
-  function onClick() {
-    setColor("blue");
-  }
-  return <div>hi</div>;
+export default function App() {
+  return (
+    <div>
+      <Counter />
+      {ReactDOM.createPortal(
+        <div>
+          <p>hi</p>
+          <p>react Programing</p>
+        </div>,
+        document.getElementById('something'),
+      )}
+    </div>
+  );
 }
