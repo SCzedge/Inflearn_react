cda883a6ab35737a73b112cc475e9d8b96a64f48
sczedge <sczedge@gmail.com>
2020-12-31 23:15:48 +0900

    12_virtual-dom end

--- 12_virtual-dom/src/App.js
+++ 12_virtual-dom/src/App.js
@@ -1,6 +1,20 @@
+import React ,{useState,useEffect} from 'react';
+import Counter from './Counter';
 
-function App() {
-  return null;
+
+export default function App() {
+  const[seconds, setSeconds] = useState(0);
+  useEffect(() =>{
+    setTimeout(()=> {
+      setSeconds(v => v+1);
+    },1000);
+  });
+  return(
+    <div>
+      {seconds % 2 === 0 && <Counter/>}
+      <h1 style={{color : seconds % 2?'red':'blue'}}>hi</h1>
+      <h2>now {seconds} is passing.</h2>
+    </div>
+  )
 }
 
-export default App;
